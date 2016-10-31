7ceac4304aac0d50bb27d0afe1fdd86b4f13a0a5
Anis Haque <anissatun30@gmail.com>
2016-10-31 10:42:35 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -13,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findViewById(R.id.imageViewSMS)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        composeSmsMessage("Pesan dari SMK Telkom Malang");
+                    }
+                });
+
 
         findViewById(R.id.imageViewPhone)
                 .setOnClickListener(new View.OnClickListener() {
@@ -31,6 +39,13 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
     }
 
+    private void composeSmsMessage(String message) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("sms_body", message);
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
     }
+}
 
 
